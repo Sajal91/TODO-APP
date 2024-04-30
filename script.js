adda547278c7ca1b933bce985e58d28c7e2aba3a
Sajal Kapoor <kapoorsajal91@gmail.com>
2024-04-30 07:01:21 +0530

    checking functionality added

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let inputBox = document.querySelector('#floatingInput');
 let addBtn = document.querySelector('#add-btn');
 let activityContainer = document.querySelector('#activity-list');
+let checkBoxesArray = [];
+let idCount = 0;
 
 addBtn.addEventListener('click',()=>{
     if(inputBox.value != "") {
@@ -12,7 +14,26 @@ addBtn.addEventListener('click',()=>{
         activity.setAttribute('class','activity');
         activityContainer.insertAdjacentElement('beforeend',activity);
         activity.appendChild(activityText);
-        activityText.innerHTML = ">> " + inputBox.value;
+
+        let checkBox = document.createElement('div');
+        let idStr = 'flexCheckDefault' + idCount;
+        checkBox.setAttribute('class','form-check');
+        checkBox.innerHTML = `
+            <input class="form-check-input" type="checkbox" value="" id="${idStr}">
+            <label class="form-check-label" for="${idStr}">
+            ${inputBox.value}
+            </label>`
+        idCount++;
+        activityText.insertAdjacentElement('afterbegin',checkBox);
+        checkBoxesArray.push(checkBox);
+        checkBoxesArray[checkBoxesArray.length - 1].firstElementChild.addEventListener('click',()=>{
+            if(checkBoxesArray[checkBoxesArray.length - 1].firstElementChild.checked == true) {
+                checkBoxesArray[checkBoxesArray.length - 1].lastChild.style.textDecoration = 'line-through';
+            } else {
+                checkBoxesArray[checkBoxesArray.length - 1].lastChild.style.textDecoration = 'none';
+            }
+        })
+
         activity.appendChild(deleteBtn);
         deleteBtn.addEventListener('click',()=>{
             deleteBtn.parentElement.remove()
